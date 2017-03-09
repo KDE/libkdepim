cca7828f8a68026364555d1723f77bfc19aeb9b3
Montel Laurent <montel@kde.org>
2017-03-09 07:21:00 +0100

    Exclude autotests too

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-$XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/'` -o $podir/libkdepim.pot
+$XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libkdepim.pot
 
