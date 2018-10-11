527cad8a9d93f5cd6f72a8d93ea9126d2b7ce927
Volker Krause <vkrause@kde.org>
2018-10-11 19:50:41 +0200

    License maintenance: GPLv2 -> GPLv2+
    Summary:
    This is based on https://techbase.kde.org/Projects/KDE_Relicensing for
    the individual authors. The only one not found on there is KDAB (hereby
    approved).
    
    Reviewers: dvratil
    
    Reviewed By: dvratil
    
    Subscribers: kde-pim
    
    Tags: #kde_pim
    
    Differential Revision: https://phabricator.kde.org/D16136

--- src/libkdepim/progresswidget/statusbarprogresswidget.cpp
+++ src/libkdepim/progresswidget/statusbarprogresswidget.cpp
@@ -10,8 +10,9 @@
   by Matt Koss <koss@miesto.sk>
 
   KMail is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License, version 2 or above,
-  as published by the Free Software Foundation.
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
   KMail is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
--- src/libkdepim/progresswidget/statusbarprogresswidget.h
+++ src/libkdepim/progresswidget/statusbarprogresswidget.h
@@ -9,8 +9,9 @@
   Copyright 2004 David Faure <faure@kde.org>
 
   KMail is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License, version 2
-  or above, as published by the Free Software Foundation.
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
   KMail is distributed in the hope that it will be useful, but
   WITHOUT ANY WARRANTY; without even the implied warranty of
