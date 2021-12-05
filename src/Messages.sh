26228e1c00a2f475b579fb58c69e9bb8664dc03c
Laurent Montel <montel@kde.org>
2021-12-05 13:25:02 +0100

    Fix some reuse lint warnings

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,6 @@
 #! /bin/sh
+# SPDX-FileCopyrightText: none
+# SPDX-License-Identifier: BSD-3-Clause
+
 $XGETTEXT `find . -name '*.h' -o -name '*.cpp' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libkdepim.pot
 
